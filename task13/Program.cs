d262355607c8e51a945375566940a78f550e17d6
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-28 18:00:52 +0500

    Add task13,task15

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number > 999)
+{
+    number /= 10;  
+}
+if(number > 99 && number < 1000)
+{
+    Console.Write(number % 10);
+}
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//7 -> да
+//1 -> нет
+Console.WriteLine("Введите цифру от 1-7");
+string number = Console.ReadLine();
+if(number == "1"|| number == "2"|| number == "3"|| number == "4" || number == "5")
+{
+    Console.WriteLine ("Нет");
+}
+else if(number == "6"|| number == "7" )
+{Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine("Вы ввели не с 1 до 7");
+}
